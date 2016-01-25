ab81c2060d04c92d77ad81118200235146ceb08f
Nicholas Bishop <nicholasbishop@gmail.com>
2016-01-24 22:05:10 -0500

    Split long file

--- pybliz/__init__.py
+++ pybliz/__init__.py
@@ -1,6 +1,7 @@
 import cyglfw3 as glfw
 from OpenGL import GL as gl
 from pybliz.math3d import *
+from pybliz.shader import *
 
 glfw.WindowHint(glfw.CONTEXT_VERSION_MAJOR, 3)
 glfw.WindowHint(glfw.CONTEXT_VERSION_MINOR, 2)
@@ -38,70 +39,6 @@ def obj_remove_comment(line):
     if ind != -1:
         line = line[:ind].rstrip()
     return line
-
-
-def extract_uniforms(source):
-    for line in source.splitlines():
-        parts = line.split()
-        if len(parts) > 0 and parts[0] == 'uniform':
-            yield parts[2].rstrip(';')
-
-
-class Shader:
-    def __init__(self, path, kind):
-        self._hnd = gl.glCreateShader(kind)
-        if self._hnd == 0:
-            raise ValueError('glCreateShader failed')
-        with open(path) as rfile:
-            source = rfile.read()
-            gl.glShaderSource(self._hnd, source)
-        gl.glCompileShader(self._hnd)
-        if gl.glGetShaderiv(self._hnd, gl.GL_COMPILE_STATUS) == False:
-            log = gl.glGetShaderInfoLog(self._hnd)
-            gl.glDeleteShader(self._hnd)
-            raise RuntimeError('shader failed to compile', log)
-        self._uniforms = set(extract_uniforms(source))
-
-    def uniforms(self):
-        return self._uniforms
-
-
-class VertexShader(Shader):
-    def __init__(self, path):
-        super().__init__(path, gl.GL_VERTEX_SHADER)
-
-
-class FragmentShader(Shader):
-    def __init__(self, path):
-        super().__init__(path, gl.GL_FRAGMENT_SHADER)
-
-
-class Program:
-    def __init__(self, vertex_shader, fragment_shader):
-        self._vs = vertex_shader
-        self._fs = fragment_shader
-        self._hnd = gl.glCreateProgram()
-        if self._hnd == 0:
-            raise ValueError('glCreateProgram failed')
-        gl.glAttachShader(self._hnd, self._vs._hnd)
-        gl.glAttachShader(self._hnd, self._fs._hnd)
-        gl.glLinkProgram(self._hnd)
-        self._uniforms = {}
-        for name in self._vs.uniforms() | self._fs.uniforms():
-            self._uniforms[name] = gl.glGetUniformLocation(self._hnd, name)
-
-    def bind(self):
-        gl.glUseProgram(self._hnd)
-
-    def set_uniform(self, key, val):
-        loc = self._uniforms[key]
-        if isinstance(val, Mat4x4):
-            gl.glUniformMatrix4fv(loc, 1, False, val._array)
-
-
-class ShaderManager:
-    def __init__(self):
-        pass
 
 
 class DrawData:
