2bf0edefd822acfc5df15b23129dcc940b23deef
Nicholas Bishop <nicholasbishop@gmail.com>
2016-02-28 21:33:12 -0500

    Code cleanup

--- bel/window.py
+++ bel/window.py
@@ -7,27 +7,20 @@ from OpenGL import GL as gl
 from bel import ipc
 
 class WindowServer:
-    def __init__(self, sock):
-        logging.basicConfig(level=logging.DEBUG, format=
-                            '%(levelname)s: window process '
-                            '[%(filename)s:%(lineno)d] %(message)s')
-
+    def __init__(self, sock, glfw):
         self.conn = ipc.Conn(sock)
         self.command_buffer = None
+        self.glfw = glfw
 
-        import cyglfw3 as glfw
+    def run(self):
+        window = self.glfw.CreateWindow(640, 480, 'bel.WindowServer')
+        self.glfw.MakeContextCurrent(window)
 
-        if not glfw.Init():
-            raise RuntimeError('glfw.Init failed')
-
-        window = glfw.CreateWindow(640, 480, 'bel.WindowServer')
-        glfw.MakeContextCurrent(window)
-
-        while not glfw.WindowShouldClose(window):
+        while not self.glfw.WindowShouldClose(window):
             self.draw()
 
-            glfw.SwapBuffers(window)
-            glfw.PollEvents()
+            self.glfw.SwapBuffers(window)
+            self.glfw.PollEvents()
             msg = self.conn.read_msg_nonblocking()
             if msg is not None:
                 if hasattr(msg, 'draw'):
@@ -43,13 +36,27 @@ class WindowServer:
             self.command_buffer.draw()
 
 
+def window_server_main(server_sock):
+    logging.basicConfig(level=logging.DEBUG, format=
+                        '%(levelname)s: window process '
+                        '[%(filename)s:%(lineno)d] %(message)s')
+
+    # pylint: disable=locally-disabled,no-member
+    import cyglfw3 as glfw
+    if not glfw.Init():
+        raise RuntimeError('glfw.Init failed')
+
+    window = WindowServer(server_sock, glfw)
+    window.run()
+
+
 class WindowClient:
     def __init__(self):
         # TODO(nicholasbishop): verify that the defaults for
         # socketpair guarantee message ordering
         sock, server_sock = socketpair()
         self.conn = ipc.Conn(sock)
-        self.proc = Process(target=WindowServer, args=(server_sock,))
+        self.proc = Process(target=window_server_main, args=(server_sock,))
         self.proc.start()
 
     def send_msg(self, msg):
