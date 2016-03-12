ddefa5451644400c2beec4c745178abd7e66a138
Nicholas Bishop <nicholasbishop@gmail.com>
2016-03-11 23:32:52 -0500

    Code cleanup

--- bel/window.py
+++ bel/window.py
@@ -1,14 +1,11 @@
-import logging
+import atexit
 import os
 from socket import AF_UNIX, SOCK_STREAM, socket
 import subprocess
-
-from OpenGL import GL as gl
+import sys
 
 from bel import ipc
-from bel.shader import ShaderProgram
-from bel.buffer_object import ArrayBufferObject
-import sys
+
 class WindowClient:
     def __init__(self):
         # TODO!
@@ -18,24 +15,21 @@ class WindowClient:
         server_sock = socket(AF_UNIX, SOCK_STREAM)
         server_sock.bind(socket_path)
         server_sock.listen(1)
-        # tracelib = '/home/nicholasbishop/apitrace/build/wrappers/glxtrace.so'
-        # os.environ['LD_PRELOAD'] = tracelib
+
         cmd = []
-        cmd += ['apitrace', 'trace']
+        #cmd += ['gdb', '--eval-command', 'run', '--args']
+        #cmd += ['apitrace', 'trace']
+        #cmd += ['valgrind']
         cmd += ['venv/bin/python3', 'bel/window_process.py', socket_path]
+
         env = dict(os.environ)
         env['PYTHONPATH'] = ':'.join(sys.path)
-        #proc = subprocess.Popen(cmd, stdin=server_sock.fileno(), env=env)
+
         proc = subprocess.Popen(cmd, env=env)
+
         sock, _ = server_sock.accept()
         self.conn = ipc.Conn(sock)
-        import time
-        print(proc)
-        import atexit
-        atexit.register(lambda: proc.wait())
-        #time.sleep(10)
-        #self.proc = Process(target=window_server_main, args=(server_sock,))
-        #self.proc.start()
+        atexit.register(proc.wait)
 
     def send_msg(self, msg):
         self.conn.send_msg(msg)
--- bel/window_process.py
+++ bel/window_process.py
@@ -1,7 +1,5 @@
 from socket import AF_UNIX, SOCK_STREAM, socket
 import logging
-import os
-import subprocess
 
 import sys
 from OpenGL import GL as gl
@@ -10,8 +8,9 @@ from bel import ipc
 from bel.shader import ShaderProgram
 from bel.buffer_object import ArrayBufferObject
 
-def handle_dbg_msg(source, msg_type, msg_id, msg_severity, msg_length, msg, user_data):
-    print('here')
+def cb_handle_dbg_msg(*args):
+    msg = args[5]
+    logging.info('glDebugMessage: %s', msg.decode())
 
 class WindowServer:
     def __init__(self, sock, glfw):
@@ -27,7 +26,8 @@ class WindowServer:
         window = self.glfw.CreateWindow(640, 480, 'bel.WindowServer')
         self.glfw.MakeContextCurrent(window)
 
-        gl.glDebugMessageCallback(gl.GLDEBUGPROC(handle_dbg_msg), None)
+        callback = gl.GLDEBUGPROC(cb_handle_dbg_msg)
+        gl.glDebugMessageCallback(callback, None)
 
         while not self.glfw.WindowShouldClose(window):
             self.draw()
@@ -76,54 +76,39 @@ class WindowServer:
             first, count = item['range']
             # TODO
             assert item['primitive'] == 'triangles'
-            #mode = gl.GL_TRIANGLES
+            mode = gl.GL_TRIANGLES
             mode = gl.GL_POINTS
             logging.debug('glDrawArrays(%s, first=%d, count=%d',
                           mode.name, first, count)
             # TODO
-            count = 2
+            #count = 2
             with material.bind():
                 gl.glDrawArrays(mode, first, count)
 
 def window_server_main(server_sock):
-    #print('verify', os.environ['LD_PRELOAD'])
     logging.basicConfig(level=logging.INFO, format=
                         '%(levelname)s: window process '
                         '[%(filename)s:%(lineno)d] %(message)s')
 
     # pylint: disable=locally-disabled,no-member
-    import time
-    print(4)
     import cyglfw3 as glfw
-    print(5)
     if not glfw.Init():
-        print(7)
         raise RuntimeError('glfw.Init failed')
-    print(6)
 
     glfw.WindowHint(glfw.CONTEXT_VERSION_MAJOR, 3)
     glfw.WindowHint(glfw.CONTEXT_VERSION_MINOR, 2)
     glfw.WindowHint(glfw.OPENGL_PROFILE, glfw.OPENGL_CORE_PROFILE)
     glfw.WindowHint(glfw.OPENGL_DEBUG_CONTEXT, True)
-    #glfw.WindowHint( glfw.CLIENT_API, glfw.OPENGL_ES_API );
-    #glfw.WindowHint( glfw.CONTEXT_VERSION_MAJOR, 2 );
 
-    print(7)
     window = WindowServer(server_sock, glfw)
     window.run()
 
 
 def main():
     socket_path = sys.argv[1]
-    print(socket_path)
-    print('proc 1')
     sock = socket(AF_UNIX, SOCK_STREAM)
-    print('proc 2')
     sock.connect(socket_path)
-    print('proc 3')
-    
     window_server_main(sock)
-    print('proc 4')
 
 
 if __name__ == '__main__':
