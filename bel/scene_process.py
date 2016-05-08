a89b33ed49c52c9a0d2fb1f68e000a775d019a9c
Nicholas Bishop <nicholasbishop@gmail.com>
2016-05-08 13:29:19 -0400

    checkpoint

--- bel/scene_process.py
+++ bel/scene_process.py
@@ -1,7 +1,17 @@
 from bel.child import main
 
-def scene_main(conn):
+
+class Scene:
     pass
+
+
+def scene_main(conn):
+    running = True
+    while running:
+        # TODO: timeout?
+        msg = conn.read_msg_blocking()
+        if msg['tag'] == 'exit':
+            running = False
 
 
 if __name__ == '__main__':
