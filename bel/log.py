a85c14dfaf8a619e122574823f703f186baf27c6
Nicholas Bishop <nicholasbishop@gmail.com>
2016-05-21 15:43:29 -0400

    Update log format

--- bel/log.py
+++ bel/log.py
@@ -1,6 +1,6 @@
 import logging
 
 def configure(name, level):
-    fmt = ('%(levelname)s: {} '
+    fmt = ('%(asctime)s %(levelname)s {} '
            '[%(filename)s:%(lineno)d] %(message)s').format(name)
     logging.basicConfig(level=level, format=fmt)
