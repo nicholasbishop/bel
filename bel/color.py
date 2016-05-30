67bdac95e37db38986b4d6674d734bfc6ade2817
Nicholas Bishop <nicholasbishop@gmail.com>
2016-05-30 19:56:17 -0400

    Make Color a Sequence

--- bel/color.py
+++ bel/color.py
@@ -1,11 +1,18 @@
+from collections.abc import Sequence
 from random import random as randfloat
 
-class Color:
+class Color(Sequence):
     def __init__(self, red=0.0, green=0.0, blue=0.0, alpha=0.0):
         self.red = red
         self.green = green
         self.blue = blue
         self.alpha = alpha
+
+    def __getitem__(self, index):
+        return (self.red, self.green, self.blue, self.alpha)[index]
+
+    def __len__(self):
+        return 4
 
     def serialize(self):
         return (self.red, self.green, self.blue, self.alpha)
