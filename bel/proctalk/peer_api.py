5255b725d36c842706313f0c4169d636055a108c
Nicholas Bishop <nicholasbishop@gmail.com>
2016-05-30 17:22:32 -0400

    Code cleanup

--- bel/proctalk/peer_api.py
+++ bel/proctalk/peer_api.py
@@ -1,21 +1,23 @@
-# pylint crashes when linting this function, not sure why yet.
-# (pylint 1.5.5, astroid 1.4.5) For now just disable all checks here.
+# pylint crashes when linting the module if this file is included. It
+# doesn't crash when linting just this file though. (pylint 1.5.5,
+# astroid 1.4.5) For now just disable all checks here.
 #
 # pylint: skip-file
+
+def make_method(method_name):
+    async def method(self, *params):
+        return await self.rpc.call('_hub_dispatch', {
+            'client_id': self.client_id,
+            'method': method_name,
+            'params': params
+        })
+    return method
+
 
 def create_peer_api(api):
     def init(self, rpc, client_id):
         self.rpc = rpc
         self.client_id = client_id
-
-    def make_method(method_name):
-        async def method(self, *params):
-            return await self.rpc.call('_hub_dispatch', {
-                'client_id': self.client_id,
-                'method': method_name,
-                'params': params
-            })
-        return method
 
     members = {'__init__': init}
     for method_name in api:
