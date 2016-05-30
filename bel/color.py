095d73b15ff858bf405f63017de436233929a5b1
Nicholas Bishop <nicholasbishop@gmail.com>
2016-05-30 15:04:04 -0400

    Color cleanup and serialize

--- bel/color.py
+++ bel/color.py
@@ -1,11 +1,14 @@
 from random import random as randfloat
 
 class Color:
-    def __init__(self, r=0.0, g=0.0, b=0.0, a=0.0):
-        self.r = r
-        self.g = g
-        self.b = b
-        self.a = a
+    def __init__(self, red=0.0, green=0.0, blue=0.0, alpha=0.0):
+        self.red = red
+        self.green = green
+        self.blue = blue
+        self.alpha = alpha
+
+    def serialize(self):
+        return (self.red, self.green, self.blue, self.alpha)
 
     @classmethod
     def random(cls):
