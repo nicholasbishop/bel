b34d0e59c19544c11df8ac7df54a637c8961cb93
Nicholas Bishop <nicholasbishop@gmail.com>
2016-05-31 21:37:36 -0400

    Remove the color loop

--- adi.py
+++ adi.py
@@ -13,6 +13,6 @@ class Adi(BaseClient):
     async def on_start(self):
         self.scene = self._peers['bel.scene_client']
 
-        while self.running:
-            await self.scene.set_background_color(Color.random())
-            await sleep(0.5)
+        # while self.running:
+        #     await self.scene.set_background_color(Color.random())
+        #     await sleep(0.5)
