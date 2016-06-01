4f54f3d10bc96c70e967db14749bc2be10657953
Nicholas Bishop <nicholasbishop@gmail.com>
2016-05-31 22:34:06 -0400

    Restore buffer_object

---
+++ bel/buffer_object.py
@@ -0,0 +1,60 @@
+from contextlib import contextmanager
+from ctypes import c_void_p
+import logging
+
+from OpenGL.GL import (GL_ARRAY_BUFFER, GL_STATIC_DRAW, glBindBuffer,
+                       glBufferData, glDeleteBuffers, glGenBuffers,
+                       glVertexAttribPointer)
+import OpenGL.GL as gl
+
+class BufferObject:
+    def __init__(self, kind):
+        self._kind = kind
+        self._hnd = glGenBuffers(1)
+
+        # TODO
+        self._vao = gl.glGenVertexArrays(1)
+
+        logging.info('glGenBuffers(1) -> %d', self._hnd)
+        if self._hnd == 0:
+            raise ValueError('glGenBuffers failed')
+
+    def release(self, conn):
+        if self._hnd is not None:
+            conn.send_msg(lambda: glDeleteBuffers(self._hnd))
+
+    @contextmanager
+    def bind(self):
+        glBindBuffer(self._kind, self._hnd)
+        try:
+            yield
+        finally:
+            pass
+            #glBindBuffer(self._kind, 0)
+
+    def set_data(self, data, usage=None):
+        if usage is None:
+            usage = GL_STATIC_DRAW
+
+        with self.bind():
+            glBufferData(self._kind, data, usage)
+            logging.info('glBufferData(buffer=%s, %s, ..., %s)', self._hnd,
+                         self._kind.name, usage.name)
+
+    def bind_to_attribute(self, attr_index, components, gltype,
+                          normalized, stride, offset):
+        with self.bind():
+            # TODO
+            gl.glBindVertexArray(self._vao)
+
+            glVertexAttribPointer(attr_index,
+                                  components,
+                                  gltype,
+                                  normalized,
+                                  stride,
+                                  c_void_p(offset))
+
+
+class ArrayBufferObject(BufferObject):
+    def __init__(self):
+        super().__init__(GL_ARRAY_BUFFER)
