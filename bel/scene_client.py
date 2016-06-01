a3fc603943a296a2d9783f50e2a1b570a7e6817a
Nicholas Bishop <nicholasbishop@gmail.com>
2016-05-31 22:21:36 -0400

    checkpoint

--- bel/scene_client.py
+++ bel/scene_client.py
@@ -1,12 +1,26 @@
 from bel.client import BaseClient, expose
 from bel.event import MouseButtonEvent
 from bel.mesh import Mesh
+from bel.future_group import FutureGroup
 
 class SceneClient(BaseClient):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self._view = None
         self._mesh = None
+        self._future_group = FutureGroup()
+
+    def _create_flush_task(self):
+        self._future_group.create_task(self._flush())
+
+    def _flush(self):
+        # TODO
+        pass
+
+    @expose
+    def shutdown(self):
+        self._future_group.cancel_all()
+        super().shutdown()
 
     @expose
     def on_start(self):
