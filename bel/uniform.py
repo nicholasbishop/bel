d0c51aa7a9b78c1ff1f0c57e4f4839d3ca0b301b
Nicholas Bishop <nicholasbishop@gmail.com>
2016-06-14 02:49:28 -0400

    Use ndarray instead of matrix for mat4

--- bel/uniform.py
+++ bel/uniform.py
@@ -7,10 +7,7 @@ class MatrixUniform:
     def bind(self, uniform_location):
         count = 1
         transpose = False
-        if self._data.shape == (4, 4):
-            glUniformMatrix4fv(uniform_location, count, transpose, self._data)
-        else:
-            raise NotImplementedError()
+        glUniformMatrix4fv(uniform_location, count, transpose, self._data)
 
 
 class VectorUniform:
--- cgmath/matrix.py
+++ cgmath/matrix.py
@@ -1,10 +1,9 @@
 import numpy
 
-
 def new_mat4(*args):
-    assert len(args) == 16
-    return numpy.matrix([args[:4],
-                         args[4:8],
-                         args[8:12],
-                         args[12:]],
-                        dtype=numpy.float32).transpose()
+    return numpy.array((
+        args[0], args[4], args[8], args[12],
+        args[1], args[5], args[9], args[13],
+        args[2], args[6], args[10], args[14],
+        args[3], args[7], args[11], args[15],
+    ), dtype=numpy.float32)
