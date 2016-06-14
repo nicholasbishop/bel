bbb88b36b155f23f084bd456ae8d6a1e21efddd7
Nicholas Bishop <nicholasbishop@gmail.com>
2016-06-13 23:11:48 -0400

    Add vec4_from_vec2

--- cgmath/vector.py
+++ cgmath/vector.py
@@ -49,3 +49,7 @@ def vec4(x=0.0, y=0.0, z=0.0, w=0.0):
 
 def vec3_from_scalar(value):
     return vec3(value, value, value)
+
+
+def vec4_from_vec2(vec, z=0.0, w=0.0):
+    return vec4(vec[0], vec[1], z, w)
