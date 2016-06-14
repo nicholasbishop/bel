3d1df96416c1977d43030751cbdd8db0d10bc45e
Nicholas Bishop <nicholasbishop@gmail.com>
2016-06-13 22:48:40 -0400

    Add vec4

--- cgmath/vector.py
+++ cgmath/vector.py
@@ -27,10 +27,6 @@ def normalized(vec):
     return vec / magnitude(vec)
 
 
-def vec2(x=0.0, y=0.0):
-    return numpy.array((x, y), numpy.float32)
-
-
 def copy_xy(dst, src):
     dst[0:2] = src[0:2]
 
@@ -39,8 +35,16 @@ def copy_xyz(dst, src):
     dst[0:3] = src[0:3]
 
 
+def vec2(x=0.0, y=0.0):
+    return numpy.array((x, y), numpy.float32)
+
+
 def vec3(x=0.0, y=0.0, z=0.0):
     return numpy.array((x, y, z), numpy.float32)
+
+
+def vec4(x=0.0, y=0.0, z=0.0, w=0.0):
+    return numpy.array((x, y, z, w), numpy.float32)
 
 
 def vec3_from_scalar(value):
