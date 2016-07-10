21444442a5c2a8cdf1716c028dcaf816850615e2
Nicholas Bishop <nicholasbishop@gmail.com>
2016-07-10 04:14:49 -0400

    Add very simple Ray class

---
+++ cgmath/ray.py
@@ -0,0 +1,7 @@
+"""Ray defined by origin and direction."""
+
+class Ray:
+    """Ray defined by origin and direction."""
+    def __init__(self, origin, direction):
+        self.origin = origin
+        self.direction = direction
