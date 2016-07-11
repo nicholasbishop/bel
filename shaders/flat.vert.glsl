1aea6f3b9e930a9b436a0b85e22ec614ac938c07
Nicholas Bishop <nicholasbishop@gmail.com>
2016-07-10 22:40:38 -0400

    Update flat shader uniforms

--- shaders/flat.vert.glsl
+++ shaders/flat.vert.glsl
@@ -3,8 +3,9 @@
 layout(location=0) in vec3 vert_loc;
 
 uniform mat4 projection;
-uniform mat4 model_view;
+uniform mat4 camera;
+uniform mat4 model;
 
 void main() {
-    gl_Position = projection * model_view * vec4(vert_loc, 1.0f);
+    gl_Position = projection * camera * model * vec4(vert_loc, 1.0f);
 }
