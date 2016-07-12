ab29f7e615cb6746d419d45d719fbaaa97efdfeb
Nicholas Bishop <nicholasbishop@gmail.com>
2016-07-12 00:34:46 -0400

    Change flat shader to use vert cols

--- shaders/flat.frag.glsl
+++ shaders/flat.frag.glsl
@@ -1,8 +1,8 @@
 #version 330 core
 
-uniform vec4 flat_color;
+in vec4 vs_color;
 out vec4 color;
 
 void main() {
-	color = flat_color;
+	color = vs_color;
 }
--- shaders/flat.vert.glsl
+++ shaders/flat.vert.glsl
@@ -1,11 +1,15 @@
 #version 330 core
 
 layout(location=0) in vec3 vert_loc;
+layout(location=1) in vec4 vert_col;
+
+out vec4 vs_color;
 
 uniform mat4 projection;
 uniform mat4 camera;
 uniform mat4 model;
 
 void main() {
+	vs_color = vert_col;
     gl_Position = projection * camera * model * vec4(vert_loc, 1.0f);
 }
